e7609acdc1c3842cddd8cd8c2a85c6b8f43d3986
Dmytro Sayko <saykodmytro@icloud.com>
2023-10-31 09:36:40 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import ImageGallery from './ImageGallery/ImageGallery';
+import toast, { Toaster } from 'react-hot-toast';
 import Searchbar from './Searchbar/Searchbar';
 import css from './App.module.css';
 import fetchPhotos from '../API/pixabay-api';
@@ -10,18 +11,23 @@ export default class App extends Component {
   state = {
     query: '',
     page: 1,
-    images: null,
+    images: [],
     loading: false,
     error: false,
     perPage: 12,
-    hasMorePages: true,
+    btnLoadMore: false,
   };
 
   handleSearch = e => {
     e.preventDefault();
     const form = e.currentTarget;
     const query = form.search.value.trim().toLowerCase();
+    if (query === '') {
+      toast.error('Sorry, there are no images matching your search query.');
+      return;
+    }
     this.setState({ query: query, page: 1, images: null });
+    form.reset();
   };
 
   async componentDidUpdate(_, prevState) {
@@ -29,45 +35,60 @@ export default class App extends Component {
       prevState.query !== this.state.query ||
       prevState.page !== this.state.page
     ) {
-      this.setState({ loading: true, error: null });
       try {
-        const data = await fetchPhotos(this.state.query, this.state.page);
+        this.setState({ loading: true });
+        const { query, page } = this.state;
+        const resp = await fetchPhotos(query, page);
+
+        const newImages = resp.hits.map(
+          ({ id, webformatURL, largeImageURL, tags }) => ({
+            id,
+            webformatURL,
+            largeImageURL,
+            tags,
+          })
+        );
         this.setState(prevState => ({
-          images: [...(prevState.images || []), ...data.hits],
-          loading: false,
+          images: [...(prevState.images || []), ...newImages],
         }));
+
+        if (resp.totalHits !== 0 && this.state.page === 1) {
+          toast.success(`Hooray! We found ${resp.totalHits} images!`);
+        }
+        const totalPage = Math.ceil(resp.totalHits / this.state.perPage);
+
+        if (totalPage > page) {
+          this.setState({ btnLoadMore: true });
+        } else if (totalPage === page && resp.totalHits) {
+          toast.error("Sorry, but you've reached the end of search results.");
+          this.setState({ btnLoadMore: false });
+        }
       } catch (error) {
-        this.setState({ error, loading: false });
+        toast.error(error.message);
+      } finally {
+        this.setState({ loading: false });
       }
     }
   }
 
   handleLoadMore = () => {
-    this.setState(
-      prevState => ({ page: prevState.page + 1 }),
-      this.smoothScroll()
-    );
-  };
-
-  smoothScroll = () => {
-    window.scrollBy({
-      top: document.documentElement.clientHeight,
-      behavior: 'smooth',
-    });
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   render() {
-    const { images, loading, hasMorePages } = this.state;
-    // const shouldShowLoadMore = images && images.length > 0 && !loading;
+    const { images, loading } = this.state;
 
     return (
       <div className={css.app}>
         {loading && <Loader />}
         <Searchbar onSubmit={this.handleSearch} />
         {images && images.length > 0 && <ImageGallery images={images} />}
-        {hasMorePages && images && images.length > 0 && (
-          <Button onClick={this.handleLoadMore} />
-        )}
+        {this.state.btnLoadMore && <Button onClick={this.handleLoadMore} />}
+        <Toaster
+          autoClose={3000}
+          position="top-right"
+          containerClassName="text-base"
+        />
       </div>
     );
   }
